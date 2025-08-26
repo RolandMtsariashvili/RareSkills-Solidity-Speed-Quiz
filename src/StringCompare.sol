6f8b2e11a58ebf663a6b8d2e6dbe757665c379e9
RolandMtsariashvili <rolandmcariashvili@gmail.com>
2025-08-26 23:24:52 +0400

    Solve stringConcat, StringCompare, RemoveElement and MemoryArray

--- src/StringCompare.sol
+++ src/StringCompare.sol
@@ -2,9 +2,10 @@
 pragma solidity 0.8.28;
 
 contract StringCompare {
-    function compare(string memory a, string memory b) public returns (bool) {
+    function compare(string memory a, string memory b) public pure returns (bool) {
         // compare two strings
         // return true if they are equal
         // return false if they are not equal
+        return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
     }
 }
