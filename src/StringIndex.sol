7cfb5310d29f06b9d6b6ed97f25242e3c40e8c82
RolandMtsariashvili <rolandmcariashvili@gmail.com>
2025-08-27 21:02:34 +0400

    StringIndex solve

--- src/StringIndex.sol
+++ src/StringIndex.sol
@@ -2,8 +2,12 @@
 pragma solidity 0.8.28;
 
 contract StringIndex {
-    function main(string memory str, uint256 index) public returns (string memory) {
+    function main(string memory str, uint256 index) public pure returns (string memory) {
         // return the character at index in str
         // assume str only consists of ascii characters
+        bytes memory result = new bytes(1);
+        result[0] = bytes(str)[index];
+
+        return string(result);
     } 
 }
