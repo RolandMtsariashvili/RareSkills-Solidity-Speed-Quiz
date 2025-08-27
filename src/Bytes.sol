ecfbf6c36f2ca642e92f53119cd4efaf99160055
RolandMtsariashvili <rolandmcariashvili@gmail.com>
2025-08-27 20:52:48 +0400

    complete Bytes

--- src/Bytes.sol
+++ src/Bytes.sol
@@ -2,14 +2,11 @@
 pragma solidity 0.8.28;
 
 contract Bytes {
-    function main(uint8 z) public returns (bytes memory) {
-        // if z = 0, return empty bytes
-        // if z = 1, return bytes 0x00
-        // if z = 2, return bytes 0x0001
-        // if z = 3, return bytes 0x000102
-        // if z = 4, return bytes 0x00010203
-        // ...
-        // etc
-
+    function main(uint8 z) public pure returns(bytes memory) {
+        bytes memory b = new bytes(z);
+        for (uint8 i = 0; i < z; i++) {
+            b[i] = bytes1(i);
+        }
+        return b;
    }
 }
