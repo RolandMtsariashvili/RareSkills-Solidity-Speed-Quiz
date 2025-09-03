167e7cc9cfc8a03fd73b74f3dd71268c46e47fa7
RolandMtsariashvili <rolandmcariashvili@gmail.com>
2025-09-03 17:15:10 +0400

    complete lowLevelArgs1

--- src/LowLevelArgs1.sol
+++ src/LowLevelArgs1.sol
@@ -1,11 +1,18 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.8.28;
 
+interface IRare { 
+    function rare(uint256) external;
+}
+
 contract LowLevelArgs1 {
     function main(address a, uint256 x) public {
         // call rare(x) using a low-level call
         // if the low level call reverts, revert also
-
+        (bool ok, ) = a.call(abi.encodeWithSignature("rare(uint256)", x));
+        if (!ok) revert();
         // bonus challenge: use an interface and a high level call to accomplish the same task
+        
+        // IRare(a).rare(x);
     }
-}
+} 
