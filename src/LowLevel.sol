a83b185f429354679308f64feb5061568f91dea7
RolandMtsariashvili <rolandmcariashvili@gmail.com>
2025-09-03 16:15:34 +0400

    complete LowLevel

--- src/LowLevel.sol
+++ src/LowLevel.sol
@@ -1,14 +1,19 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.8.28;
 
+interface IFoo {
+    function foo() external;
+}
+
 contract LowLevel {
     function main(address a) public returns (bool) {
-        // call function "foo()" on address a
-        // do not use an interface, use a low level call
-        // return true if the call succeeded
-        // return false if the call failed
+        (bool ok,) = a.call(abi.encodeWithSignature("foo()"));
+        return ok;
 
-        // bonus challenge: use an interface and a high level call to accomplish the same task
-
+        // try IFoo(a).foo() {
+        //     return true;
+        // } catch {
+        //     return false;
+        // }
     }
 }
