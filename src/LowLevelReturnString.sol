f3941dab0cd5dbcbd7522ad1c3b490040a0436fd
RolandMtsariashvili <rolandmcariashvili@gmail.com>
2025-09-03 20:29:42 +0400

    finish LowLevelReturnString

--- src/LowLevelReturnString.sol
+++ src/LowLevelReturnString.sol
@@ -1,13 +1,27 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.8.28;
 
+interface IBaz {
+    function baz() external returns (string memory result );
+}
+
 contract LowLevelReturnString {
     function main(address a) public returns (string memory) {
         // call function "baz()" on address a
         // do not use an interface
         // baz() returns a string. Return the string.
         // However, if baz reverts, return an empty string 
+        (bool ok, bytes memory data) = a.call(abi.encodeWithSignature("baz()"));
+        if (!ok) {
+            return "";
+        }
 
+        return abi.decode(data, (string));
         // bonus challenge: use an interface and a high level call to accomplish the same task
+        // try IBaz(a).baz() returns (string memory result) {
+        //     return result;
+        // } catch {
+        //     return "";
+        // }
     }
 }
