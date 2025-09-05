e51ca626cd893a923a8875685f4ea1421e8ac107
RolandMtsariashvili <rolandmcariashvili@gmail.com>
2025-09-05 14:32:54 +0400

    complete NoDelegateCall

--- src/NoDelegateCall.sol
+++ src/NoDelegateCall.sol
@@ -1,14 +1,22 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.28;
+import {console} from 'forge-std/console.sol';
 
 contract NoDelegateCall {
     // your code here
     // hint: https://www.rareskills.io/post/nodelegatecall
 
+    address private immutable _self;
+
+    constructor() {
+        _self = address(this);
+    }
+
     function meaningOfLifeAndEverything() public view returns (uint256 fourtyTwo) {
         // your code here
         // this function should be callable, but not delegatecallable
         // it should return 42
+        require(address(this) == _self); 
         fourtyTwo = 42;
     }
 }
